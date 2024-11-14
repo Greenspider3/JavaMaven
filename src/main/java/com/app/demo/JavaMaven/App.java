53edfcf814de6740c88b83828303d06f5ae9b0f1
Nanditha <nandithakonduri@gmail.com>
2024-11-14 12:19:41 +0530

    Sample Maven Java

---
+++ src/main/java/com/app/demo/JavaMaven/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.JavaMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Nanditha-22BD1A054Y");
+    }
+}
